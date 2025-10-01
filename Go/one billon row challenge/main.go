e54580bb0f88ca7a6b7b59ee5eca7da2903a76cb
Mário Becker <mario.roberto1@hotmail.com>
2025-09-30 21:11:49 -0300

    obrc part1

--- Go/one billon row challenge/main.go
+++ Go/one billon row challenge/main.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+)
+
+type Measurements struct {
+	Nome  string
+	Min   float64
+	Max   float64
+	Sum   float64
+	Count int64
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	measurements, err := os.Open("meansurement.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer measurements.Close()
+	scanner := bufio.NewScanner(measurements)
+
+	for scanner.Scan() {
+		rawData := scanner.Text()
+		semicolonIndex := strings.Index(rawData, ";")
+		location := rawData[:semicolonIndex]
+		temperature := rawData[semicolonIndex+1:]
+
+		fmt.Println(location, temperature)
+		return
+	}
+
 }
